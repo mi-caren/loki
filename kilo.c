652254c5963df6ed3010bcbc2d18c9e0efc21097
Michele Carenza <michele.carenza@proton.me>
2024-04-13 16:46:00 +0200

    ctrl-q to quit

--- kilo.c
+++ kilo.c
@@ -5,6 +5,9 @@
 #include <termios.h>
 #include <unistd.h>
 
+#define CTRL_KEY(k)    ((k) & 0x1f)
+
+
 struct termios orig_termios;
 
 /*
@@ -87,7 +90,7 @@ int main() {
         } else {
             printf("%*d (%c)\r\n", 3, c, c);
         }
-        if (c == 'q') break;
+        if (c == CTRL_KEY('q')) break;
     }
     return 0;
 }
