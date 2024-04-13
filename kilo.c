6e3219fd62fe5a82b0ab9de0d28a9871694d2cca
Michele Carenza <michele.carenza@proton.me>
2024-04-13 15:02:36 +0200

    disable raw mode

--- kilo.c
+++ kilo.c
@@ -1,6 +1,32 @@
+#include <termios.h>
 #include <unistd.h>
 
+/*
+ * Legge gli attributi del terminale,
+ * ne modifica alcuni e riscrive gli attributi.
+ */
+void enable_raw_mode() {
+    struct termios raw;
+    // Legge gli attributi del terminale nella struct raw
+    tcgetattr(STDIN_FILENO, &raw);
+    // disabilita l'echoing: ciò che si digita
+    // non saràstampato a terminale
+    // Flags:
+    //    c_lflag -> local flags
+    //    c_iflag -> input flags
+    //    c_oflag -> output flags
+    //    c_cflag -> control flags
+    raw.c_lflag &= ~(ECHO);
+    // scrive il nuovo valore della struct raw
+    // TCSAFLUSH specifica quindo devono essere applicate le modifich:
+    //    aspetta che tutti gli output siano stati scritti sul terminale
+    //    e scarta tutti gli input non letti
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main() {
+    enable_raw_mode();
+
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
