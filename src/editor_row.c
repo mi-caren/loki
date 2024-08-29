298994201bcbcd51c93a98eca15c753659b2e380
Michele Carenza <michele.carenza@proton.me>
2024-08-29 14:01:10 +0200

    render digits purple

--- src/editor_row.c
+++ src/editor_row.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -14,16 +15,20 @@ int editorRowRender(struct EditorRow *row)
 {
     unsigned int i;
     unsigned int tabs = 0;
+    unsigned int digits = 0;
     for (i = 0; i < row->size; i++) {
-        if (row->chars[i] == '\t')
+        if (row->chars[i] == '\t') {
             tabs++;
+        } else if (isdigit(row->chars[i])) {
+            digits++;
+        }
     }
 
     // eventrully free render if it is not null
     // this makes the munction more general because it can be called
     // also to RE-rende a row
     free(row->render);
-    char *new = malloc(row->size + 1 + tabs*(TAB_SPACE_NUM - 1));
+    char *new = malloc(row->size + 1 + tabs*(TAB_SPACE_NUM - 1) + digits*10);
 
     if (new == NULL)
         return -1;
@@ -36,6 +41,12 @@ int editorRowRender(struct EditorRow *row)
             unsigned int tab_i;
             for (tab_i = 0; tab_i < TAB_SPACE_NUM; tab_i++)
                 row->render[j++] = ' ';
+        } else if (isdigit(row->chars[i])) {
+            memcpy(&row->render[j], "\x1b[95m", 5);
+            j += 5;
+            row->render[j++] = row->chars[i];
+            memcpy(&row->render[j], "\x1b[39m", 5);
+            j += 5;
         } else {
             row->render[j++] = row->chars[i];
         }
