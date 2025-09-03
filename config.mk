39654b0ec7a2bebeba5353a23cb901843b463347
Michele Carenza <michele@micaren.org>
2025-09-03 16:42:14 +0200

    create configure.sh script

--- config.mk
+++
@@ -1,8 +0,0 @@
-CC = gcc
-AR = ar
-
-INSTALL_DIR = /usr/local
-
-CPPFLAGS = -MMD -Isrc
-CFLAGS = -Wall -Wextra -pedantic --std=c23
-LDFLAGS = -L./build -laeolus
---
+++ configure.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+CC=cc
+AR=ar
+INSTALL_DIR=/usr/local/bin
+LIB_DIR=/usr/local/lib
+
+usage() {
+    cat << EOF
+Usage: $0 [OPTION]...
+Create config.mk file
+
+OPTIONS:
+    -c      Set compiler
+    -a      Set archiver
+    -i      Set install directory
+    -l      Set linker search directory
+    -h      Display this help message
+EOF
+}
+
+while getopts 'c:a:i:l:h' OPT; do
+    case $OPT in
+        c)
+            CC=$OPTARG
+            ;;
+        a)
+            AR=$OPTARG
+            ;;
+        i)
+            INSTALL_DIR=$OPTARG
+            ;;
+        l)
+            LIB_DIR=$OPTARG
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        ?)
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+cat << EOF > config.mk
+CC = $CC
+AR = $AR
+INSTALL_DIR = $INSTALL_DIR
+LIB_DIR = $LIB_DIR
+EOF
